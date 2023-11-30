3c1f98a95dcd2a1a0484bba60896ef59d62de36a
AntoRodriguez <arodriguez@mecaris.com>
2023-11-30 19:22:10 -0300

    case_id y date

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,6 +13,7 @@ class BookingsController < ApplicationController
   def create_booking_maker
     provider_type = params[:provider_type].capitalize
     provider_id = params[:provider_id]
+    case_id = params[:case_id]
     start_date = params[:start_date]
     end_date = params[:end_date]
     booking = Booking.new(provider_type: provider_type, provider_id: provider_id, start_date: start_date, end_date: end_date)
@@ -26,9 +27,11 @@ class BookingsController < ApplicationController
   def create_booking_material
     @status = true
     @materials = params[:materials]
+    @array = [] # crea un array vacío
     @materials.each do |m|  
       provider_type = m[:provider_type].capitalize
       provider_id = m[:provider_id]
+      case_id = m[:case_id]
       start_date = m[:start_date]
       end_date = m[:end_date]
       delivery_place = m[:delivery_place].downcase.strip
@@ -37,13 +40,42 @@ class BookingsController < ApplicationController
       quantity = m[:quantity]
       booking = Booking.new(provider_type: provider_type, provider_id: provider_id, start_date: start_date, end_date: end_date, material: material_id, quantity: quantity, delivery_place: delivery_place)   
       @status = false  unless booking.save
+      @hash = {} # crea un hash vacío
+      @hash["case_id"] = case_id
+      @hash["date"] = end_date
+      @array.push(@hash) # agrega el hash al array
     end   
     if @status      
-      render_success({}, {}, CREATED)
+      render_success(@array, {}, CREATED) # devuelve el array con el estado
     else
       render_error("Error", booking.errors, INTERNAL_SERVER_ERROR)
     end
-  end 
+  end
+
+  
+
+  #def create_booking_material
+  #  @status = true
+  #  @materials = params[:materials]
+  #  @materials.each do |m|  
+  #    provider_type = m[:provider_type].capitalize
+  #    provider_id = m[:provider_id]
+  #    case_id = m[:case_id]
+  #    start_date = m[:start_date]
+  #    end_date = m[:end_date]
+  #    delivery_place = m[:delivery_place].downcase.strip
+  #    material_name = m[:material].downcase
+  #    material_id = Material.find_by(name: material_name)
+  #    quantity = m[:quantity]
+  #    booking = Booking.new(provider_type: provider_type, provider_id: provider_id, start_date: start_date, end_date: end_date, material: material_id, quantity: quantity, delivery_place: delivery_place)   
+  #    @status = false  unless booking.save
+  #  end   
+  #  if @status      
+  #    render_success({}, {}, CREATED)
+  #  else
+  #    render_error("Error", booking.errors, INTERNAL_SERVER_ERROR)
+  #  end
+  #end 
   
    
 
