20a92567ec2a8ab57e749950cda9c5b8d02f7760
AntoRodriguez <arodriguez@mecaris.com>
2023-11-30 21:09:22 -0300

    case_id y date....Bc

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -63,35 +63,9 @@ class BookingsController < ApplicationController
 
   def state 
     options = ["continuar", "fallo", "termino"]
-    options.sample
+    res = options.sample
+    render json: res, status: OK
   end 
-
-  
-
-  #def create_booking_material
-  #  @status = true
-  #  @materials = params[:materials]
-  #  @materials.each do |m|  
-  #    provider_type = m[:provider_type].capitalize
-  #    provider_id = m[:provider_id]
-  #    case_id = m[:case_id]
-  #    start_date = m[:start_date]
-  #    end_date = m[:end_date]
-  #    delivery_place = m[:delivery_place].downcase.strip
-  #    material_name = m[:material].downcase
-  #    material_id = Material.find_by(name: material_name)
-  #    quantity = m[:quantity]
-  #    booking = Booking.new(provider_type: provider_type, provider_id: provider_id, start_date: start_date, end_date: end_date, material: material_id, quantity: quantity, delivery_place: delivery_place)   
-  #    @status = false  unless booking.save
-  #  end   
-  #  if @status      
-  #    render_success({}, {}, CREATED)
-  #  else
-  #    render_error("Error", booking.errors, INTERNAL_SERVER_ERROR)
-  #  end
-  #end 
-  
-   
 
   def delete
     @p = Provisions.all
